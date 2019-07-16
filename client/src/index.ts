3b0b56813ad339839459a158b67ae83efb6e5944
Ferid HELALI <helaliferid@gmail.com>
2019-07-16 21:21:05 +0100

    first Local MediaStream Sample

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,10 +1,31 @@
-function component() {
-    const element = document.createElement('div');
-  
-    
-    element.innerHTML = 'Hello World';
-  
-    return element;
+//navigator.getUserMedia=navigator.getUserMedia //|| navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+
+// Use constraint to ask for video and/or audio
+const constraints={
+  audio:true,
+  video:true
+}
+
+const video = document.querySelector('video');
+
+// Callback to be called in case of success...
+
+function successCallback(stream:any){
+  //window.stream=stream 
+  console.log(stream);
+  // Note: make the returned stream available to console for inspection
+  if(window.URL){
+    video.srcObject=stream;
+  }else{
+    video
   }
-  
-  document.body.appendChild(component());
+   
+  video.play();
+}
+
+function errorCallback(error:any){
+  console.log('navigator.getUserMedia error:',error)
+}
+
+navigator.getUserMedia(constraints,successCallback,errorCallback);
